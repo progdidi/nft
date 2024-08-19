721a151b65caa4b9e419ff61cbe3777206f6e9f8
Diana Sosnovskaya <progdidi@gmail.com>
2024-08-19 20:03:05 +0300

    feat: add footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './footer.scss';
+
+const Footer = () => {
+    return ( 
+        <footer className="footer">
+            <div className="container">
+
+            </div>
+        </footer>
+     );
+}
+ 
+export default Footer;
