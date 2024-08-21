7cc0c71bb4dcd9cd1b042a82cdf258fffadb28de
Diana Sosnovskaya <progdidi@gmail.com>
2024-08-21 12:28:04 +0300

    feat: add helpers creators

---
+++ src/helpers/creators/creators.js
@@ -0,0 +1,96 @@
+import id1 from './images/1.png';
+import id2 from './images/1.png';
+import id3 from './images/1.png';
+import id4 from './images/1.png';
+import id5 from './images/1.png';
+import id6 from './images/1.png';
+import id7 from './images/1.png';
+import id8 from './images/1.png';
+import id9 from './images/1.png';
+import id10 from './images/1.png';
+
+const creators = [
+    {
+        name: 'Jaydon Ekstrom Bothman',
+        img: id1, 
+        id: 1,
+        sold: 800, 
+        volume: 12.4,
+    },
+
+    {
+        name: 'Ruben Carder',
+        img: id2, 
+        id: 2,
+        sold: 800, 
+        volume: 12.4,
+    },
+
+    {
+        name: 'Alfredo Septimus',
+        img: id3, 
+        id: 3,
+        sold: 800, 
+        volume: 12.4,
+    },
+
+    {
+        name: 'Davis Franci',
+        img: id4, 
+        id: 4,
+        sold: 800, 
+        volume: 12.4,
+    },
+
+    {
+        name: 'Livia Rosser',
+        img: id5, 
+        id: 5,
+        sold: 800, 
+        volume: 12.4,
+    },
+
+    {
+        name: 'Kianna Donin',
+        img: id6, 
+        id: 6,
+        sold: 800, 
+        volume: 12.4,
+    },
+
+    {
+        name: 'Phillip Lipshutz',
+        img: id7, 
+        id: 7,
+        sold: 800, 
+        volume: 12.4,
+    },
+
+    {
+        name: 'Maria Rosser',
+        img: id8, 
+        id: 8,
+        sold: 800, 
+        volume: 12.4,
+    },
+
+    {
+        name: 'Kianna Stanton',
+        img: id9, 
+        id: 9,
+        sold: 800, 
+        volume: 12.4,
+    },
+
+    {
+        name: 'Angel Lubin',
+        img: id10, 
+        id: 10,
+        sold: 800, 
+        volume: 12.4,
+    }
+
+
+]
+
+export {creators}
