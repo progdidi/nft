c202c83c07572c352a05c35c596001b3536e64c7
Diana Sosnovskaya <progdidi@gmail.com>
2024-08-21 11:18:47 +0300

    feat: feat rankings page

--- src/pages/RankingsPage/RankingsItem/RankingsItem.jsx
+++ src/pages/RankingsPage/RankingsItem/RankingsItem.jsx
@@ -1,6 +1,19 @@
 const RankingsItem = () => {
     return (
-        <div className="rankings-item"></div>
+        <div className="rankings-item">
+            <div className="rankings-item__number">1</div>
+            <div className="rankings-item__artist">
+                <img src="" alt="" className="rankings-item__img" />
+                <p className="rankings-item__name"></p>
+            </div>
+            <p className="rankings-item__change">
+            +1.41%
+            </p>
+            <div className="rankings-item__sold">
+            602
+            </div>
+            <div className="rankings-item__volume">12.4 ETH</div>
+        </div>
     )
 }
 
